d87a44db40d0f838ce3de17c69c0a0c6d6db5776
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-27 07:08:14 -0400

    Added Variables Package

---
+++ src/com/hamilton/variables/variables.java
@@ -0,0 +1,8 @@
+package com.hamilton.variables;
+
+public class variables {
+    public static void main(String[] args) {
+        int number = 100;
+        String brand = "Hamilton Multimedia";
+    }
+}
