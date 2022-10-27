7fe975d243fe9e0704d6fb45d36fc7862e51524f
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-27 07:11:15 -0400

    Added a few primitive types

--- src/com/hamilton/variables/variables.java
+++ src/com/hamilton/variables/variables.java
@@ -1,8 +1,13 @@
 package com.hamilton.variables;
 
+import java.util.Date;
+
 public class variables {
     public static void main(String[] args) {
+        // Types of variables
         int number = 100;
+        double Number = 100.01;
         String brand = "Hamilton Multimedia";
+        Date date = new Date();
     }
 }
