ef525df5dbfc4c14ec70bd01f3c8ec98a1046ff5
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-29 19:41:50 -0400

    Added LocalDate Class

--- src/com/hamilton/seconds/LocalDate.java
+++ src/com/hamilton/seconds/LocalDate.java
@@ -1,5 +1,15 @@
 package com.hamilton.seconds;
 
-public class LocalDate {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class LocalDateClass {
     // Code this class
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+    }
 }
