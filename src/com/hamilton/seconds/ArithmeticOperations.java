828928e4778e70c33b76a324bba5c86cd0141e65
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-29 23:49:59 -0400

    Added Arithmetic Operations

---
+++ src/com/hamilton/seconds/ArithmeticOperations.java
@@ -0,0 +1,16 @@
+package com.hamilton.seconds;
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        // Arithmetic Operations
+        System.out.println(10 + 2 + 90);
+        System.out.println(10 - 2);
+        System.out.println(10 * 2);
+        System.out.println(10 % 3);
+
+        //Bodmas Bodies, orders, division, multiplication, addition, subtraction
+        System.out.println(10 * (2 + 7));
+        System.out.println(10 % 3);
+    }
+}
