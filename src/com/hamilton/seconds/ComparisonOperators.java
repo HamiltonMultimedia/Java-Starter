135e4ad3e0cecdabd4152e46dc6d1e1ae1a3357d
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-30 00:11:41 -0400

    Added Boolean Expressions

---
+++ src/com/hamilton/seconds/ComparisonOperators.java
@@ -0,0 +1,19 @@
+package com.hamilton.seconds;
+
+public class ComparisonOperators {
+
+    public static void main(String[] args) {
+        // Comparison Operators or Boolean Expressions
+        int lillyAge = 9;
+        int trainAge = 8;
+        System.out.println(lillyAge > trainAge);
+        System.out.println(lillyAge >= trainAge);
+        System.out.println(lillyAge < trainAge);
+        System.out.println(lillyAge <= trainAge);
+        System.out.println(lillyAge == trainAge);
+        System.out.println(lillyAge != trainAge);
+
+    }
+
+
+}
