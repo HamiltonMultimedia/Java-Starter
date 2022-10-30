0067d5c9a2b32a883f852ecbc7692d4878b2e15e
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-29 23:52:41 -0400

    Added Math Methods

---
+++ src/com/hamilton/seconds/MathMethods.java
@@ -0,0 +1,15 @@
+package com.hamilton.seconds;
+
+public class MathMethods {
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(6, 10));
+        System.out.println(Math.max(6, -10));
+        System.out.println(Math.pow(5.0, 2.0));
+        // (int) will "cast" the float to an integer
+        System.out.println((int) Math.pow(5.0, 2.0));
+        System.out.println((int) Math.sqrt(25));
+        System.out.println(Math.PI);
+    }
+}
