fd63ec55bf19b97a3ff1de6df25efe85c8d78215
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-30 00:45:54 -0400

    Added Logical Operators

---
+++ src/com/hamilton/seconds/LogicalOperators.java
@@ -0,0 +1,20 @@
+package com.hamilton.seconds;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+        // Logical Operators
+        boolean isAdult = false;
+        boolean isStudent = false;
+        boolean isHamiltonMediaMember = true;
+        System.out.println(isAdult && isStudent);
+        System.out.println(isAdult || isStudent);
+
+        System.out.println((isAdult || isStudent) && isHamiltonMediaMember);
+        // The exclamation "!" represents the opposite logical value
+        System.out.println((!isAdult || isStudent) && !isHamiltonMediaMember);
+        System.out.println(!isAdult);
+        String name = "Terence";
+        System.out.println(10 > 8 || 2 <= 2 && isAdult && name.contains("T"));
+    }
+}
