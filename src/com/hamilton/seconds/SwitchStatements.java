e9b3cc8a7b100ae50b5f16daba6c1ca46f478a4e
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-30 01:52:30 -0400

    Added Switch Statement

---
+++ src/com/hamilton/seconds/SwitchStatements.java
@@ -0,0 +1,20 @@
+package com.hamilton.seconds;
+
+public class SwitchStatements {
+    public static void main(String[] args) {
+        // Switch Statement
+        String gender = "FEMALE";
+        switch (gender) {
+            case "FEMALE":
+                System.out.println("I am a female");
+                break;
+            case "MALE":
+                System.out.println("I am a male");
+                break;
+            case "PREFER_NOT_SAY":
+                System.out.println("Prefer not say");
+                break;
+            default:
+        }
+    }
+}
