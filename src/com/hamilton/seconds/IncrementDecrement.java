3b222254db1b45b90252cedf6ec89f6367e61d20
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-02 09:02:50 -0400

    Added Increment and Decrement

---
+++ src/com/hamilton/seconds/IncrementDecrement.java
@@ -0,0 +1,20 @@
+package com.hamilton.seconds;
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        // Symbols "++" and "--" are increment and decrement operators
+        int [] numbers = {2, 0, 1, 4, 100, 4, 90, 78, 77};
+
+        // Counts up with increment
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        // Counts down with decrement
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
