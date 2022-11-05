f948dd98574aa7245ca8a4dbfcccc423d07083e5
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-05 11:22:06 -0400

    Added Methods Class

---
+++ src/com/hamilton/seconds/Methods.java
@@ -0,0 +1,16 @@
+package com.hamilton.seconds;
+
+public class Methods {
+    // Methods only run where they are called
+    // They are used to perform some kind of logic
+    // Built-in and User-defined are available to be used
+    public static void main(String[] args) {
+        // The println() method is built-in
+        System.out.println("Hello");
+        String brand = "Samsung";
+        System.out.println(brand.toUpperCase());
+        System.out.println(brand.startsWith("S"));
+        System.out.println(brand.endsWith("g"));
+    }
+
+}
