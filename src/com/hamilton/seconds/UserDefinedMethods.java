3b0218fd16f752031a1bcd2c6862904bb3c20950
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-05 12:12:19 -0400

    Updated User Defined Method

--- src/com/hamilton/seconds/UserDefinedMethods.java
+++ src/com/hamilton/seconds/UserDefinedMethods.java
@@ -1,16 +1,25 @@
 package com.hamilton.seconds;
 
+import java.util.Arrays;
+
 public class UserDefinedMethods {
 
     public static void main(String[] args) {
         // Methods
         char[] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D'};
-        int count = countOccurrences(letters);
+        // Method Invocation or usage, extracted into a variable.
+        int count = countOccurrences(letters, 'D');
         System.out.println(count);
     }
 
-    public static int countOccurrences(char [] letters) {
-        System.out.println("method() countOccurrences was invoked");
-        return -1;
+    // countOccurrences() Method Definition
+    public static int countOccurrences(char [] letters, char searchLetter) {
+        int count = 0;
+        for (char letter : letters) {
+            if (letter == searchLetter) {
+                count++;
+            }
+        }
+        return count;
     }
 }
