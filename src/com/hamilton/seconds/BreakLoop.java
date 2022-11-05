0c840f5d6583ffb170209db4ac9d63b39c75ec7b
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-05 09:01:39 -0400

    Added While Loop

--- src/com/hamilton/seconds/BreakLoop.java
+++ src/com/hamilton/seconds/BreakLoop.java
@@ -5,12 +5,22 @@ public class BreakLoop {
     public static void main(String[] args) {
         // Break and continue
         String[] names = {"Anna", "Ali", "Bob", "Mike"};
-        for (String name : names) {
-            if (name.equals("Bob")) {
-                // Break keyword "breaks" or ends the loop
-                break;
-            }
+//        for (String name : names) {
+//            if (name.equals("Bob")) {
+//                // Break keyword "breaks" or ends the loop
+//                break;
+//            }
+//            System.out.println(name);
+//        }
+
+        for (String name : names){
+           if (name.startsWith("A")){
+               // Continue keyword "continues" back to the top of the loop
+               continue;
+           }
             System.out.println(name);
         }
+
+
     }
 }
---
+++ src/com/hamilton/seconds/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.hamilton.seconds;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        // While Loop
+        int count = 0;
+
+        while (count <= 20) {
+            System.out.println("Count " + count);
+            count++;
+        }
+    }
+}
